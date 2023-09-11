714f54666ecdd1635c8bf694e7858a453a28011a
jurecapuder <capuder.j@gmail.com>
2023-09-11 22:45:25 +0200

    Creating an [id] file

---
+++ app/job-details/[id].js
@@ -0,0 +1,38 @@
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components";
+import { COLORS, icons, SIZES } from "../../constants";
+import useFetch from "../../hook/useFetch";
+
+const JobDetails = () => {
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch('job-details', {
+    job_id: params.id
+  })
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen></Stack.Screen>
+    </SafeAreaView>
+  )
+}
+
+export default JobDetails
