5a1690a51a408656b7ca36c125e3e6969903bc3d
jurecapuder <capuder.j@gmail.com>
2023-09-11 23:13:51 +0200

    Updating company tab

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -13,10 +13,27 @@ const Company = ({ companyLogo, jobTitle, companyName, location }) => {
           source={{
             uri: checkImageURL(companyLogo)
               ? companyLogo
-              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg"
           }}
           style={styles.logoImage}
         />
+      </View>
+
+      <View style={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
+
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{companyName} / </Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            resizeMode='contain'
+            style={styles.locationImage}
+          />
+
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
       </View>
     </View>
   )
