0ba2723224b090b91a93b98e369be2bb852746db
jurecapuder <capuder.j@gmail.com>
2023-09-12 15:37:02 +0200

    Adding specifics

--- components/jobdetails/specifics/Specifics.jsx
+++ components/jobdetails/specifics/Specifics.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text } from 'react-native';
 
-import styles from './specifics.style'
+import styles from './specifics.style';
 
-const Specifics = () => {
+const Specifics = ({ title, points }) => {
   return (
-    <View>
-      <Text>Specifics</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}:</Text>
     </View>
   )
 }
