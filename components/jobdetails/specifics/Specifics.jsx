add1fc5233ddc9c9275756d6260203f092d662f6
jurecapuder <capuder.j@gmail.com>
2023-09-12 16:02:55 +0200

    Implementing specifics details

--- components/jobdetails/specifics/Specifics.jsx
+++ components/jobdetails/specifics/Specifics.jsx
@@ -7,6 +7,15 @@ const Specifics = ({ title, points }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}:</Text>
+
+      <View style={styles.pointsContainer}>
+        {points.map((item, index) => (
+          <View style={styles.pointWrapper} key={item + index}>
+            <View style={styles.pointDot} />
+            <Text style={styles.pointText}>{item}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   )
 }
