8bcc0f156239cef0a7edbd5e7e37f38c24a97137
jurecapuder <capuder.j@gmail.com>
2023-09-12 15:37:29 +0200

    Updating id's

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -35,6 +35,20 @@ const JobDetails = () => {
 
   const onRefresh = () => {};
 
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return <Specifics
+          title="Qualifications"
+          points={data[0].job_highlights?.qualifications ?? ["N/A"]}
+        />
+      case "About":
+      case "Responsibilites":
+      default:
+        break;
+    }
+  }
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -45,50 +59,54 @@ const JobDetails = () => {
           headerLeft: () => (
             <ScreenHeaderBtn
               iconUrl={icons.left}
-              dimension="60%"
+              dimension='60%'
               handlePress={() => router.back()}
             />
           ),
           headerRight: () => (
             <ScreenHeaderBtn
               iconUrl={icons.share}
-              dimension="60%"
+              dimension='60%'
             />
           ),
-          headerTitle: ""
+          headerTitle: "",
         }}
-      >
-        <>
-          <ScrollView
-            showsVerticalScrollIndicator={false}
-            refreshControl={
-              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-            }>
-              {isLoading ? (
-                <ActivityIndicator size="large" color={COLORS.primary}/>
-              ) : error ? (
-                <Text>Something went wrong</Text>
-              ) : data.length === 0 ? (
-                <Text>No data</Text>
-              ) : (
-                <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
-                  <Company
-                    companyLogo={data[0].employer_logo}
-                    jobTitle={data[0].job_title}
-                    companyName={data[0].employer_name}
-                    location={data[0].job_country}
-                  />
+      />
 
-                  <JobTabs
-                  
-                  />
-                </View>
-              )}
-          </ScrollView>
-        </>
-      </Stack.Screen>
+      <>
+        <ScrollView showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+        >
+          {isLoading ? (
+            <ActivityIndicator size='large' color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data available</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+
+              {displayTabContent()}
+            </View>
+          )}
+        </ScrollView>
+
+      </>
     </SafeAreaView>
-  )
+  );
 }
 
 export default JobDetails
